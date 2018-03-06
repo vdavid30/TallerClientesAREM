7fdc262584baca8f1647ec2e8fb5f56f03a0997f
David Vaca <david.vaca@mail.escuelaing.edu.co>
2018-03-06 13:47:27 -0500

    Date Datagram CComplete

--- DateDatagramClient/src/datedatagramclient/DatagramTimeClient.java
+++ DateDatagramClient/src/datedatagramclient/DatagramTimeClient.java
@@ -12,43 +12,38 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 public class DatagramTimeClient {
     private int segundos = 0;
-    private static Timer timer; 
-    class Contador extends TimerTask {
-        public void run() {
-            segundos++;
-            System.out.println("segundo: " + segundos);
-        }
-    }
-    public static void main(String[] args) {
-        byte[] sendBuf = new byte[256];
-        try {
-            timer = new Timer();
-            timer.schedule(new Contador(), 0, 1000);
-            
-            
-            
-            DatagramSocket socket = new DatagramSocket();
-            byte[] buf = new byte[256];
-            InetAddress address = InetAddress.getByName("127.0.0.1");
-            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
-            socket.send(packet);
-            packet = new DatagramPacket(buf, buf.length);
-            socket.receive(packet);
-            String received = new String(packet.getData(), 0, packet.getLength());
-            System.out.println("Date: " + received);
-            
-            packet = new DatagramPacket(buf, buf.length, address, 4445);                        
-            socket.send(packet);
-            packet = new DatagramPacket(buf, buf.length);
-            socket.receive(packet);
-            received = new String(packet.getData(), 0, packet.getLength());
-            System.out.println("Date: " + received);
-        } catch (SocketException ex) {
-            Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (UnknownHostException ex) {
-            Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
+    private static String received = "";
+    
+    public static void main(String[] args) throws InterruptedException {
+        while(true){
+       
+            byte[] sendBuf = new byte[256];
+            try {
+                DatagramSocket socket = new DatagramSocket();
+                byte[] buf = new byte[256];
+                InetAddress address = InetAddress.getByName("127.0.0.1");
+                DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
+                socket.send(packet);
+                packet = new DatagramPacket(buf, buf.length);
+                socket.setSoTimeout(1000);            
+                try{
+                    
+                    socket.receive(packet);
+                    received = new String(packet.getData(), 0, packet.getLength());                 
+                    System.out.println("Date: " + received);
+                }catch(Exception e){
+                    socket.close();
+                    System.out.println("Date: " + received);
+                }                                    
+                Thread.sleep(5000);
+
+            } catch (SocketException ex) {
                 Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (UnknownHostException ex) {
+                Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(DatagramTimeClient.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 }
